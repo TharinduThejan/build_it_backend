c98efcc2d8644575f56e4123e24165565762feda
TharinduThejan <141844415+TharinduThejan@users.noreply.github.com>
2025-12-24 16:09:01 +0530

    feat: add MongoDB and NestJS configuration with necessary dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRoot(process.env.MONGODB_URI || '', {
+      connectionFactory: (connection: Connection) => {
+        console.log('✅ MongoDB connected successfully');
+        return connection;
+      },
+    }),
+
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
