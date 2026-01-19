d2338a4c752d0dedea8b3a7028d9a79d49a51383
TharinduThejan <141844415+TharinduThejan@users.noreply.github.com>
2026-01-19 10:55:11 +0530

    feat: enhance product management with role-based access control and add quantity field to product schema

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -2,6 +2,7 @@ export class CreateProductDto {
   productId?: number;
   name!: string;
   price!: number;
+  qty!: number;
   category!: string;
   image!: string;
 }
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -6,8 +6,12 @@ import {
   Post,
   Put,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/roles.decorator';
 
 @Controller('products')
 export class ProductsController {
@@ -21,16 +25,25 @@ export class ProductsController {
   async getProductById(@Param('id') productId: string) {
     return await this.productsService.findOne(productId);
   }
+
   @Post()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin')
   async createProduct(@Body() newproductdata: any) {
     const newProduct = await this.productsService.create(newproductdata);
     return newProduct;
   }
+
   @Put('/:id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin')
   async updateProduct(@Param('id') productId: string, @Body() updateData: any) {
     return await this.productsService.update(productId, updateData);
   }
+
   @Delete('/:id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin')
   async deleteProduct(@Param('id') productId: string) {
     return await this.productsService.remove(productId);
   }
